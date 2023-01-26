453db8b6be21f493a6a7d338df1a7555ea150a83
sjhdev <sjh.developer1@gmail.com>
2023-01-26 05:48:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vite-project
+ React-Meme-Generator
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Components/Header.jsx'
+import Meme from './Components/Meme.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
+}
+
+export default App
